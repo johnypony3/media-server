8445954c96bed0b0dcbbee795cab3b452b3e58b3
johnypony3 <edward@theemm.com>
2017-05-31 04:58:26 -0700

    wip

---
+++ copy.sh
@@ -0,0 +1,3 @@
+
+scp edward@nas:"/mnt/DroboFS/Shares/DroboApps/plex/Library/Plex\ Media\ Server/Preferences.xml" /Users/edward/git/media-server/config/plex-config/
+#scp -r edward@nas:"/mnt/DroboFS/Shares/DroboApps/plex/Library/Plex\ Media\ Server" /Users/edward/git/media-server/backup/plex-backup/
